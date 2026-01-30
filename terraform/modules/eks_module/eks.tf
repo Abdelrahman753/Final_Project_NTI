58b62c56c59a3155b6537fa60999afe2b5cab839
Abdelrahman753 <abdelrahman.m.27.22@gmail.com>
2026-01-30 18:29:51 +0200

    Add EKS module with cluster, node group, IAM roles, and outputs

---
+++ terraform/modules/eks_module/eks.tf
@@ -0,0 +1,106 @@
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = var.eks_cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  version  = var.eks_cluster_version
+  
+
+  vpc_config {
+    subnet_ids = var.subnet_ids
+    
+    endpoint_private_access = true
+    endpoint_public_access  = true
+
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSServicePolicy,
+  ]
+
+  tags = {
+    Environment = var.env
+  }
+}
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name = var.eks_cluster_name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+
+resource "aws_eks_node_group" "node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "${var.eks_cluster_name}-node-group"
+  node_role_arn   = aws_iam_role.node_group_role.arn
+  subnet_ids      = var.subnet_ids
+
+  scaling_config {
+    desired_size = var.desired_capacity
+    max_size     = var.max_capacity
+    min_size     = var.min_capacity
+  }
+  instance_types = var.instance_types
+
+
+
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node_group_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node_group_AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node_group_AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
+
+resource "aws_iam_role" "node_group_role" {
+  name = "${var.eks_cluster_name}-node-group-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node_group_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_group_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_group_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node_group_role.na
---
+++ terraform/modules/eks_module/variables.tf
@@ -0,0 +1,40 @@
+variable "eks_cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "eks_cluster_version" {
+  description = "Version of the EKS cluster"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for the EKS cluster"
+  type        = list(string)
+}
+
+variable "env" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "desired_capacity" {
+  description = "Desired number of nodes in the EKS node group"
+  type        = number
+}
+
+variable "max_capacity" {
+  description = "Maximum number of nodes in the EKS node group"
+  type        = number
+}
+
+variable "min_capacity" {
+  description = "Minimum number of nodes in the EKS node group"
+  type        = number
+}
+variable "instance_types" {
+  description = "List of instance types for the EKS node group"
+  type        = list(string)
+}
+
+
