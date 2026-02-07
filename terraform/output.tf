f1911ba23893466c0297a491c5ff6b169f4782c0
Abdelrahman753 <abdelrahman.m.27.22@gmail.com>
2026-02-07 21:19:11 +0200

    Adds ALB controller IRSA role ARN output

--- terraform/output.tf
+++ terraform/output.tf
@@ -9,3 +9,7 @@ output "nlb_target_group_arn" {
 output "api_endpoint" {
   value = module.api_gateway_module.api_endpoint
 }
+
+output "alb_controller_irsa_role_arn" {
+  value = module.eks_module.alb_controller_irsa_role_arn
+}
