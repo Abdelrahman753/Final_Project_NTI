4abd416735ee15ed7cbd27cf7216b3ba8838040f
Abdelrahman753 <abdelrahman.m.27.22@gmail.com>
2026-02-07 20:23:39 +0200

    Update Terraform outputs to match renamed modules

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,11 +1,11 @@
 output "eks_cluster_name" {
-  value = module.eks_module.cluster_name
+  value = module.eks_module.eks_cluster_name
 }
 
 output "nlb_target_group_arn" {
-  value = module.nlb.target_group_arn
+  value = module.nlb_module.target_group_arn
 }
 
 output "api_endpoint" {
-  value = try(module.apigw.api_endpoint, null)
+  value = module.api_gateway_module.api_endpoint
 }
